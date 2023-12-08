5635efb39d74877f701f9a793c5156f3235dbf64
AkshayShineKrishna <akshayshinekrishna@gmail.com>
2023-12-08 21:56:11 +0530

    added area

---
+++ NP/Area - Client Server Communication using Sockets/Client.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.net.*;
+
+class Client {
+
+    public static void main(String arg[]) {
+        BufferedReader b, b1;
+        String s1;
+        PrintWriter p;
+        try {
+            Socket s = new Socket("localhost", 8000);
+            while (true)
+            {
+
+                b = new BufferedReader(new InputStreamReader(System.in));
+                b1 = new BufferedReader(new InputStreamReader(s.getInputStream()));
+                p = new PrintWriter(s.getOutputStream(), true);
+                System.out.println("Enter radius:");
+                s1 = b.readLine();
+                p.println(s1);
+                String s2 = b1.readLine();
+                if (s2.compareTo("exit") == 0) {
+                    System.exit(0);
+                }
+                System.out.println("Area of circle from server : " + s2);
+            }
+        } catch (Exception e) {
+            
+            System.out.println(e);
+        }
+    }
+}
---
+++ NP/Area - Client Server Communication using Sockets/Server.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.net.*;
+
+class Server {
+
+    public static void main(String arg[]) {
+        BufferedReader b;
+        String s1;
+        PrintWriter p;
+        try {
+            Socket c;
+            ServerSocket s = new ServerSocket(8000);
+            c = s.accept();
+            while (true)
+
+            {
+                b = new BufferedReader(new InputStreamReader(c.getInputStream()));
+                p = new PrintWriter(c.getOutputStream(), true);
+                s1 = b.readLine();
+                if (s1.compareTo("exit") == 0) {
+                    p.println("exit");
+                    System.out.println("Server Stopped");
+                    System.exit(0);
+                }
+                double d = Double.parseDouble(s1);
+                double a = 3.14 * d * d;
+                p.println(a);
+                System.out.println("Area sent to client");
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
